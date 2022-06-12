fdee160d1491cc341cb69ce022b88c7d8a96f5a2
Osada97 <osadamanohara55@gmail.com>
2022-06-13 04:54:10 +0530

    Added navbar color change when scroll

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+//change nav bar color on scroll
+const nav = document.querySelector("#navbar");
+let scrollPosition = 0;
+let scrollStatus = false;
+
+window.addEventListener("load", () => {
+  scrollStatus = true;
+});
+
+window.addEventListener("scroll", () => {
+  scrollPosition = window.scrollY;
+  console.log(scrollStatus);
+
+  if (scrollPosition > 50 && scrollStatus) {
+    nav.classList.add("navcolor-csoz");
+  } else {
+    nav.classList.remove("navcolor-csoz");
+  }
+});
