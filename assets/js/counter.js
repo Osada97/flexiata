375c4847cd884e11aff89993240abde1ff7e4b44
Osada97 <osadamanohara55@gmail.com>
2022-06-19 09:16:23 +0530

    added counter animation

---
+++ assets/js/counter.js
@@ -0,0 +1,36 @@
+const counterSection = document.querySelector("#counter");
+const counters = document.querySelectorAll(".counter");
+const speed = 200;
+
+//use for active counter animation when counter section viewable for client
+const observer = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    activeCounter();
+  }
+});
+
+observer.observe(counterSection);
+
+const activeCounter = () => {
+  console.log("active");
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText.split(" ")[0];
+
+      if (count < target) {
+        const value = count + 1;
+        if (counter.classList.contains("plus")) {
+          counter.innerText = `${Math.ceil(value)} +`;
+        } else {
+          counter.innerText = `${Math.ceil(value)}`;
+        }
+        setTimeout(updateCount, 1);
+      } else {
+        count.innerText = target;
+      }
+    };
+
+    updateCount();
+  });
+};
