9d53be9b2fb0659c8ffa56e78db4eb927c03a995
Osada97 <osadamanohara55@gmail.com>
2022-06-30 23:32:53 +0530

    added home page animations and added footer navigation

--- assets/js/animation/main.js
+++ assets/js/animation/main.js
@@ -19,6 +19,38 @@ const oServices = gsap.timeline({
     markers: { startColor: "blue", endColor: "black" },
   },
 });
+const oServicesGrid = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-section",
+    start: "25% 80%",
+    end: "100%",
+    markers: { startColor: "yellow", endColor: "black" },
+  },
+});
+const whyChoose = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".fourth-section",
+    start: "top 80%",
+    end: "0%",
+    markers: { startColor: "red", endColor: "red" },
+  },
+});
+const ourWork = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".sixth-section",
+    start: "top 80%",
+    end: "0%",
+    markers: { startColor: "blue", endColor: "red" },
+  },
+});
+const letsStart = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".eight-section",
+    start: "top 80%",
+    end: "0%",
+    markers: { startColor: "blue", endColor: "red" },
+  },
+});
 
 // !!hero section animation
 // hero main text animation
@@ -53,11 +85,11 @@ tl.fromTo(".banner-gsp", { opacity: 0 }, { opacity: 1, duration: 3 }, "<40%");
 
 tl.fromTo(".mouse-gsp", { opacity: 0 }, { opacity: 1 }, "<10%");
 
-// !!counter animation
+// !!customer animation
 counterTl.fromTo(
   ".counter-gsp",
   { y: "30", opacity: 0 },
-  { y: "0", opacity: 1, duration: 0.5, stagger: 0.5 }
+  { y: "0", opacity: 1, duration: 0.5, stagger: 0.4 }
 );
 
 // !!Our services animation
@@ -68,7 +100,7 @@ oServices.fromTo(
     x: "0%",
     opacity: 1,
     delay: 0.5,
-    duration: 3,
+    duration: 2,
     ease: Power4.easeOut,
   }
 );
@@ -85,13 +117,105 @@ oServices.fromTo(
 oServices.fromTo(
   ".sebanner-gsp",
   { opacity: 0 },
-  { opacity: 1, duration: 3 },
+  { opacity: 1, duration: 2 },
   "<10%"
 );
 
 oServices.fromTo(
   ".sebgimg-gsp",
   { y: "-50" },
-  { y: 0, ease: Elastic.easeOut.config(1, 0.7), duration: 3 },
+  { y: 0, ease: Elastic.easeOut.config(1, 0.7), duration: 2 },
+  "<"
+);
+
+oServicesGrid.fromTo(
+  ".colani-gsp",
+  { y: "-20" },
+  { y: 0, ease: Elastic.easeOut.config(1, 0.7), duration: 2, stagger: 0.5 },
+  "<5%"
+);
+
+// !!why choose us animation
+whyChoose.fromTo(
+  ".secmain-gsp",
+  { y: "-50", scale: 0.9 },
+  { y: 0, scale: 1, duration: 1, delay: 0.5, stagger: 0.1 }
+);
+
+whyChoose.fromTo(
+  ".whybut-gsp",
+  { y: "-50" },
+  { y: 0, duration: 1, stagger: 0.2 },
+  "<"
+);
+whyChoose.fromTo(
+  ".imgwhy-gsp",
+  { opacity: 0, scale: 0.8 },
+  { opacity: 1, scale: 1, duration: 0.5, stagger: 0.2 }
+);
+whyChoose.fromTo(
+  ".secwhy-gsp",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.5, stagger: 0.2 },
+  "<"
+);
+
+//!! our work section animation
+ourWork.fromTo(
+  ".ourTitle-gsp",
+  { x: "-50%", opacity: 0 },
+  {
+    x: "0%",
+    opacity: 1,
+    delay: 0.5,
+    duration: 2,
+    ease: Power4.easeOut,
+  }
+);
+ourWork.fromTo(
+  ".ourpara-gsp",
+  { opacity: 0 },
+  {
+    opacity: 1,
+    duration: 3,
+  },
+  "<50%"
+);
+ourWork.fromTo(
+  ".bgimg-gsp",
+  { opacity: 0, scale: 0.9 },
+  { opacity: 1, scale: 1, duration: 2 },
+  "<"
+);
+ourWork.fromTo(
+  ".ourbanner-gsp",
+  { opacity: 0 },
+  { opacity: 1, duration: 2 },
+  "<10%"
+);
+ourWork.fromTo(
+  ".ourworkimgrid-gsp",
+  { opacity: 0.9, scale: 0.95 },
+  { opacity: 1, scale: 1, duration: 1, stagger: 0.5 },
+  "<10%"
+);
+ourWork.fromTo(".serbut-gsp", { y: "20" }, { y: "0", duration: 1 }, "<10%");
+
+// !!lets start section
+letsStart.fromTo(
+  ".letsMainc-gsp",
+  { scale: 0.9 },
+  { scale: 1, ease: Elastic.easeOut.config(1, 0.3), delay: 0.5, duration: 2 }
+);
+letsStart.fromTo(
+  ".bgimglet-gsp",
+  { opacity: 0 },
+  { opacity: 1, duration: 2 },
+  "<10%"
+);
+letsStart.fromTo(
+  ".letbanner-gsp",
+  { opacity: 0 },
+  { opacity: 1, duration: 2 },
   "<"
 );
