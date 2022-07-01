fa72e48d78f9fcca854959710324eb479b23c3c6
Osada97 <osadamanohara55@gmail.com>
2022-07-02 00:20:19 +0530

    added portfolio page animation

---
+++ assets/js/animation/portfoliopage.js
@@ -0,0 +1,97 @@
+const tlse = gsap.timeline({
+  defaults: { duration: 0.75, ease: Power3.easeOut },
+});
+
+const containsc = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".contsc-gsp",
+    start: "top 80%",
+    end: "0%",
+    markers: { startColor: "blue", endColor: "red" },
+  },
+});
+
+const relatedwork = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".related-gsp",
+    start: "top 80%",
+    end: "0%",
+    markers: { startColor: "blue", endColor: "red" },
+  },
+});
+
+//!!services hero section
+tlse.fromTo(
+  ".mainText-gsp",
+  { x: "-50%", opacity: 0 },
+  {
+    x: "0%",
+    opacity: 1,
+    delay: 0.5,
+    duration: 3,
+    stagger: 0.2,
+    ease: Power4.easeOut,
+  }
+);
+tlse.fromTo(
+  ".butani-gsp",
+  { scale: 0.8, opacity: 0 },
+  {
+    scale: 1,
+    opacity: 1,
+    duration: 1,
+    ease: Power4.easeOut,
+  },
+  "<50%"
+);
+tlse.fromTo(
+  ".webimg-gsp",
+  { scale: 0.8, opacity: 0 },
+  { scale: 1, opacity: 1, duration: 1 },
+  "<"
+);
+
+tlse.fromTo(".banner-gsp", { opacity: 0 }, { opacity: 1, duration: 3 }, "<40%");
+
+// !!paragraph animation
+containsc.fromTo(
+  ".contsc-gsp",
+  {
+    x: -80,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+  }
+);
+
+// !!related works animation
+relatedwork.fromTo(
+  ".relati-gsp",
+  {
+    x: -80,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    stagger: 0.3,
+    duration: 1,
+  }
+);
+relatedwork.fromTo(
+  ".gridrela-gsp",
+  {
+    y: -20,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    stagger: 0.3,
+    duration: 1,
+  },
+  "<30%"
+);
